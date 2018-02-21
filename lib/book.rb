868fbcac09a84e22229441f88c2a7f604d84feb0
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-21 13:47:12 -0800

    add book.all method to pass spec.

--- lib/book.rb
+++ lib/book.rb
@@ -8,6 +8,14 @@ class Book
   end
 
   def self.all
-    
+    returned_books = DB.exec("SELECT * FROM books;")
+    books = []
+    returned_books.each() do |book|
+      id = book.fetch("id").to_i
+      title = book.fetch("title")
+      author = book.fetch("author")
+      books.push(Book.new({:id => id, :title => title, :author => author}))
+    end
+    books
   end
 end
